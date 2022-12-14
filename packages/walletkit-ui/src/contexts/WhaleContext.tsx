9c7c33c4a3de027dc7b72e56afa09e1114420b92
Lyka <lykalabrada@gmail.com>
2022-12-14 12:18:27 +0800

    feature(setup): define url inside whale context

--- packages/walletkit-ui/src/contexts/WhaleContext.tsx
+++ packages/walletkit-ui/src/contexts/WhaleContext.tsx
@@ -1,10 +1,9 @@
-import { useServiceProviderContext } from "@contexts/StoreServiceProvider";
 import {
   WhaleApiClient,
   WhaleApiClientOptions,
   WhaleRpcClient,
 } from "@defichain/whale-api-client";
-import { EnvironmentNetwork } from "@waveshq/wallet-core";
+import { EnvironmentNetwork } from "@waveshq/walletkit-core";
 import React, { createContext, useContext, useMemo } from "react";
 
 import { useNetworkContext } from "./NetworkContext";
@@ -14,23 +13,33 @@ const WhaleApiClientContext = createContext<{
   whaleRPC: WhaleRpcClient;
 }>(undefined as any);
 
-function newOceanOptions({
-  url,
-  network,
-}: {
-  network: EnvironmentNetwork;
-  url: string;
-}): WhaleApiClientOptions {
+function newOceanOptions(network: EnvironmentNetwork): WhaleApiClientOptions {
   switch (network) {
-    case EnvironmentNetwork.MainNet:
-      return { url, network: "mainnet", version: "v0" };
-    case EnvironmentNetwork.TestNet:
-      return { url, network: "testnet", version: "v0" };
-    case EnvironmentNetwork.RemotePlayground:
-      return { url, network: "regtest", version: "v0" };
     case EnvironmentNetwork.LocalPlayground:
+      return {
+        url: "http://localhost:19553",
+        network: "regtest",
+        version: "v0",
+      };
+    case EnvironmentNetwork.RemotePlayground:
+      return {
+        url: "https://playground.jellyfishsdk.com",
+        network: "regtest",
+        version: "v0",
+      };
+    case EnvironmentNetwork.TestNet:
+      return {
+        url: "https://testnet.ocean.jellyfishsdk.com",
+        network: "testnet",
+        version: "v0",
+      };
+    case EnvironmentNetwork.MainNet:
     default:
-      return { url, network: "regtest", version: "v0" };
+      return {
+        url: "https://ocean.defichain.com",
+        network: "mainnet",
+        version: "v0",
+      };
   }
 }
 
@@ -56,13 +65,12 @@ export function WhaleProvider({
   children,
 }: React.PropsWithChildren<any>): JSX.Element | null {
   const { network } = useNetworkContext();
-  const { url } = useServiceProviderContext();
   const client = useMemo(
     () => ({
-      whaleAPI: newWhaleAPIClient(newOceanOptions({ network, url })),
-      whaleRPC: newWhaleRpcClient(newOceanOptions({ network, url })),
+      whaleAPI: newWhaleAPIClient(newOceanOptions(network)),
+      whaleRPC: newWhaleRpcClient(newOceanOptions(network)),
     }),
-    [network, url]
+    [network]
   );
 
   return (
