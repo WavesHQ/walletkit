cefe656d456f1f4b9d0d8be9f789ac01db6d70bc
JJ Adonis <work@josejai.me>
2022-12-16 16:28:15 +0800

    feature(walletkit-ui): setup playground context

--- packages/walletkit-core/src/api/playground.ts
+++ packages/walletkit-core/src/api/playground.ts
@@ -1,19 +1,19 @@
-import {
-  PlaygroundApiClient,
-} from "@defichain/playground-api-client";
+import { PlaygroundApiClient } from "@defichain/playground-api-client";
 
-import {EnvironmentNetwork} from "./environment";
-import {getDefaultDefiChainURL} from "./whale";
+import { EnvironmentNetwork } from "./environment";
+import { getDefaultDefiChainURL } from "./whale";
 
-export function newPlaygroundClient(network: EnvironmentNetwork): PlaygroundApiClient {
-  const url = getDefaultDefiChainURL(network)
+export function newPlaygroundClient(
+  network: EnvironmentNetwork
+): PlaygroundApiClient {
+  const url = getDefaultDefiChainURL(network);
   switch (network) {
     case EnvironmentNetwork.RemotePlayground:
       return new PlaygroundApiClient({
         url,
       });
     case EnvironmentNetwork.LocalPlayground:
-      return new PlaygroundApiClient({url});
+      return new PlaygroundApiClient({ url });
     default:
       throw new Error(`playground not available for '${network}'`);
   }
