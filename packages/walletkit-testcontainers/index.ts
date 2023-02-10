8307f07e6a263fb2818f2bff3963cc573d9aa6f8
veralygit <57183851+veralygit@users.noreply.github.com>
2023-02-10 15:09:59 +0800

    prettier

--- packages/walletkit-testcontainers/index.ts
+++ packages/walletkit-testcontainers/index.ts
@@ -1,2 +1,1 @@
 export * from "./../walletkit-testcontainers/src";
-
--- packages/walletkit-testcontainers/src/DeFiChainStubContainer.unit.ts
+++ packages/walletkit-testcontainers/src/DeFiChainStubContainer.unit.ts
@@ -1,9 +1,8 @@
-import {DeFiChainStubContainer} from "./DeFiChainStubContainer";
-import {waitForExpect} from "@birthdayresearch/sticky-jest";
+import { DeFiChainStubContainer } from "./DeFiChainStubContainer";
 
-describe("testcontainers initialize", ()=>{
-  it("should initialize containers", async () =>{
-    const container = new DeFiChainStubContainer()
-    expect(container instanceof DeFiChainStubContainer).toBe(true)
-  })
-})
+describe("testcontainers initialize", () => {
+  it("should initialize containers", async () => {
+    const container = new DeFiChainStubContainer();
+    expect(container instanceof DeFiChainStubContainer).toBe(true);
+  });
+});
