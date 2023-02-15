cfbdf5db307ba6f686fcc7837532d96bd69409a7
Jordan Kee <jordan.kee@cakedefi.com>
2023-02-15 16:15:34 +0800

    Prettier

--- packages/walletkit-ui/src/data/exchanges.ts
+++ packages/walletkit-ui/src/data/exchanges.ts
@@ -12,75 +12,75 @@ import Swyftx from "@assets/images/exchanges/Swyftx.png";
 import Transak from "@assets/images/exchanges/Transak.png";
 
 export interface ExchangeProps {
-    image: string;
-    name: string;
-    url: string;
+  image: string;
+  name: string;
+  url: string;
 }
 
 export const exchanges: ExchangeProps[] = [
-    {
-      name: "Kucoin",
-      image: Kucoin,
-      url: "https://www.kucoin.com/trade/DFI-BTC",
-    },
-    {
-      name: "Huobi",
-      image: Huobi,
-      url: "https://www.huobi.com/en-us/exchange/dfi_usdt",
-    },
-    {
-      name: "Gate.io",
-      image: Gateio,
-      url: "https://www.gate.io/trade/DFI_USDT",
-    },
-    {
-      name: "Bittrex",
-      image: Bittrex,
-      url: "https://global.bittrex.com/Market/Index?MarketName=BTC-DFI",
-    },
-    {
-      name: "Bitrue",
-      image: Bitrue,
-      url: "https://www.bitrue.com/trade/dfi_btc",
-    },
-    {
-      name: "Latoken",
-      image: Latoken,
-      url: "https://latoken.com/exchange/DFI_BTC",
-    },
-    {
-      name: "DFX",
-      image: DFX,
-      url: "https://dfx.swiss/en/",
-    },
-    {
-      name: "Transak",
-      image: Transak,
-      url: "https://global.transak.com/",
-    },
-    {
-      name: "EasyCrypto (Australia)",
-      image: EasyCrypto,
-      url: "https://easycrypto.com/au/buy-sell/dfi-defichain",
-    },
-    {
-      name: "EasyCrypto (New Zealand)",
-      image: EasyCrypto,
-      url: "https://easycrypto.com/nz/buy-sell/dfi-defichain",
-    },
-    {
-      name: "Bybit",
-      image: Bybit,
-      url: "https://www.bybit.com/en-US/trade/spot/DFI/USDT",
-    },
-    {
-      name: "Swyftx",
-      image: Swyftx,
-      url: "https://swyftx.com/au/buy/defichain/",
-    },
-    {
-      name: "Cake DeFi",
-      image: CakeDeFi,
-      url: "https://cakedefi.com/",
-    },
+  {
+    name: "Kucoin",
+    image: Kucoin,
+    url: "https://www.kucoin.com/trade/DFI-BTC",
+  },
+  {
+    name: "Huobi",
+    image: Huobi,
+    url: "https://www.huobi.com/en-us/exchange/dfi_usdt",
+  },
+  {
+    name: "Gate.io",
+    image: Gateio,
+    url: "https://www.gate.io/trade/DFI_USDT",
+  },
+  {
+    name: "Bittrex",
+    image: Bittrex,
+    url: "https://global.bittrex.com/Market/Index?MarketName=BTC-DFI",
+  },
+  {
+    name: "Bitrue",
+    image: Bitrue,
+    url: "https://www.bitrue.com/trade/dfi_btc",
+  },
+  {
+    name: "Latoken",
+    image: Latoken,
+    url: "https://latoken.com/exchange/DFI_BTC",
+  },
+  {
+    name: "DFX",
+    image: DFX,
+    url: "https://dfx.swiss/en/",
+  },
+  {
+    name: "Transak",
+    image: Transak,
+    url: "https://global.transak.com/",
+  },
+  {
+    name: "EasyCrypto (Australia)",
+    image: EasyCrypto,
+    url: "https://easycrypto.com/au/buy-sell/dfi-defichain",
+  },
+  {
+    name: "EasyCrypto (New Zealand)",
+    image: EasyCrypto,
+    url: "https://easycrypto.com/nz/buy-sell/dfi-defichain",
+  },
+  {
+    name: "Bybit",
+    image: Bybit,
+    url: "https://www.bybit.com/en-US/trade/spot/DFI/USDT",
+  },
+  {
+    name: "Swyftx",
+    image: Swyftx,
+    url: "https://swyftx.com/au/buy/defichain/",
+  },
+  {
+    name: "Cake DeFi",
+    image: CakeDeFi,
+    url: "https://cakedefi.com/",
+  },
 ];
--- packages/walletkit-ui/src/images.d.ts
+++ packages/walletkit-ui/src/images.d.ts
@@ -1,4 +1,4 @@
-declare module '*.png' {
-    const src: string
-    export default src
+declare module "*.png" {
+  const src: string;
+  export default src;
 }
