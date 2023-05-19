5c47bc0dd5147a25bfd82a33980a2967800cb6e2
joshua <josh_817@hotmail.com>
2023-05-19 13:04:51 +0800

    chore: remove unused eth address validation function

--- packages/walletkit-core/src/api/addressValidator.ts
+++ packages/walletkit-core/src/api/addressValidator.ts
@@ -1,15 +1,10 @@
 import { fromAddress } from "@defichain/jellyfish-address";
 import { NetworkName } from "@defichain/jellyfish-network";
-import { ethers } from "ethers";
 
 export const validateAddress = (
   address: string,
   network: NetworkName
 ): boolean => {
   const decodedAddress = fromAddress(address, network);
-  const isValid = decodedAddress !== undefined;
-  return isValid;
+  return decodedAddress !== undefined;
 };
-
-export const validateEthAddress = (address: string): boolean =>
-  ethers.utils.isAddress(address);
--- packages/walletkit-core/src/api/addressValidator.unit.ts
+++ packages/walletkit-core/src/api/addressValidator.unit.ts
@@ -1,17 +1,9 @@
-import { validateAddress, validateEthAddress } from "./addressValidator";
+import { validateAddress } from "./addressValidator";
 
 const WALLET_ADDRESS = {
   mainnet: "df1qldycdkvqaacwsf4gmgl29mt686mh95kattran0",
   testnet: "tf1qm5hf2lhrsyfzeu0mnnzl3zllznfveua5rprhr4",
   regtest: "bcrt1qh2yq58tzsh4gled7wv98mm7lndj9u5v6zyr2ge",
-};
-
-const ETH_ADDRESS = {
-  valid: "0x8ba1f109551bD432803012645Ac136ddd64DBA72",
-  validICAP: "XE65GB6LDNXYOFTX0NSV3FUWKOWIXAMJK36",
-  invalid: "0x8Ba1f109551bD432803012645Ac136ddd64DBa72",
-  invalidICAP: "0x8Ba1f109551bD432803012645Ac136ddd64DBA72",
-  random: "testing invalid eth address",
 };
 
 describe("Address validator", () => {
@@ -42,32 +34,6 @@ describe("Address validator", () => {
 
   it("should be invalid when a Regtest address is used on Mainnet network", () => {
     const isValidAddress = validateAddress(WALLET_ADDRESS.regtest, "mainnet");
-    expect(isValidAddress).toBeFalsy();
-  });
-
-  // eth addresses
-  it("should be valid ETH address", () => {
-    const isValidAddress = validateEthAddress(ETH_ADDRESS.valid);
-    expect(isValidAddress).toBeTruthy();
-  });
-
-  it("should be valid ETH ICAP address", () => {
-    const isValidAddress = validateEthAddress(ETH_ADDRESS.validICAP);
-    expect(isValidAddress).toBeTruthy();
-  });
-
-  it("should be invalid ETH address", () => {
-    const isValidAddress = validateEthAddress(ETH_ADDRESS.invalid);
-    expect(isValidAddress).toBeFalsy();
-  });
-
-  it("should be invalid ETH ICAP address", () => {
-    const isValidAddress = validateEthAddress(ETH_ADDRESS.invalidICAP);
-    expect(isValidAddress).toBeFalsy();
-  });
-
-  it("should be invalid random address", () => {
-    const isValidAddress = validateEthAddress(ETH_ADDRESS.random);
     expect(isValidAddress).toBeFalsy();
   });
 });
